bed5b0109731daa6232ec689eda26f80f5e71100
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-12-15 13:42:51 -0500

    build: Add 3.13 3.14 support. Drop 3.8 3.9 support.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'agate-stats'
 copyright = '2015, Christopher Groskopf'
-version = '0.4.2'
+version = '0.4.3'
 release = version
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='agate-stats',
-    version='0.4.2',
+    version='0.4.3',
     description='agate-stats adds additional statistical methods to agate.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
@@ -21,11 +21,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Multimedia :: Graphics',
